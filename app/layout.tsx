5d934a04c0e8cf938c505d24e8e729abf81e4815
muhammed sinan <muhammedsinan7749@gmail.com>
2025-10-25 07:37:59 +0530

    []

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
+import { Metadata } from "next";
 import DashboardLayout from "@/components/DashboardLayout";
 import Providers from "@/components/layout/Providers";
 import { fontMap, getAllFontVariables } from "@/lib/fonts";
 import { getSelectedColorScheme, getSelectedFont } from "@/lib/server/settings-helpers";
 import { WrapperProps } from "@/types";
+
+export const metadata: Metadata = {
+	title: "Theme Kit",
+	description: "Theme Kit is a tool for creating and managing themes for your website.",
+};
 
 const RootLayout = async ({ children }: WrapperProps) => {
 	const selectedFont = await getSelectedFont();
