d7f3ce2a71960b135cfa9e9e55002e167be89e5f
muhammed sinan <muhammedsinan7749@gmail.com>
2025-11-09 11:25:29 +0530

    Add @dodopayments/nextjs dependency and configure related environment variables in the config file

---
+++ app/checkout/route.ts
@@ -0,0 +1,15 @@
+import { Checkout } from "@dodopayments/nextjs";
+import { DODO_PAYMENTS_API_KEY, DODO_PAYMENTS_ENVIRONMENT, DODO_PAYMENTS_RETURN_URL } from "@/config";  
+
+export const GET = Checkout({
+  bearerToken: DODO_PAYMENTS_API_KEY,
+  returnUrl: DODO_PAYMENTS_RETURN_URL,
+  environment: DODO_PAYMENTS_ENVIRONMENT,
+  type: "static"
+});
+
+export const POST = Checkout({
+  bearerToken: DODO_PAYMENTS_API_KEY,
+  returnUrl: DODO_PAYMENTS_RETURN_URL,
+  environment: DODO_PAYMENTS_ENVIRONMENT
+});
--- config/index.ts
+++ config/index.ts
@@ -14,3 +14,8 @@ export const FONT_COOKIE_NAME = "preferred-font";
 export const COLOR_SCHEME_COOKIE_NAME = "COLOR_SCHEME_COOKIE_NAME";
 export const DEFAULT_COLOR_SCHEME = "default";
 export const COLOR_SCHEME_COOKIE_EXPIRY_DAYS = 365;
+
+
+export const DODO_PAYMENTS_API_KEY = process.env.DODO_PAYMENTS_API_KEY ?? "";
+export const DODO_PAYMENTS_RETURN_URL = process.env.DODO_PAYMENTS_RETURN_URL ?? "";
+export const DODO_PAYMENTS_ENVIRONMENT = (process.env.DODO_PAYMENTS_ENVIRONMENT ?? "test_mode") as "live_mode" | "test_mode";
